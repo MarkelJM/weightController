981cb22e884a69846c5e74eb82247e590a9092f1
Markel <markjuaristi@gmail.com>
2023-12-12 12:01:41 +0100

    created appstate for currentview

---
+++ WeightController/WeightController/Navigation/AppState.swift
@@ -0,0 +1,16 @@
+//
+//  AppState.swift
+//  WeightController
+//
+//  Created by Markel Juaristi on 12/12/23.
+//
+
+import Foundation
+
+class AppState: ObservableObject {
+    @Published var currentView: AppView = .login
+}
+
+enum AppView {
+    case login
+}
