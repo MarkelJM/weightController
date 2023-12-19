121df5a3818920b78ff07485b7f64f18b3d1d397
Markel <markjuaristi@gmail.com>
2023-12-19 17:25:07 +0100

    done register process

--- WeightController/WeightController/Register/ProfileRegister/ProfileRegisterViewModel.swift
+++ WeightController/WeightController/Register/ProfileRegister/ProfileRegisterViewModel.swift
@@ -5,6 +5,87 @@
 //  Created by Markel Juaristi on 15/12/23.
 //
 
+
+import Foundation
+import SwiftUI
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseFirestore
+
+class ProfileRegisterViewModel: ObservableObject {
+    @Published var firstName: String = ""
+    @Published var lastName: String = ""
+    @Published var address: String = ""
+    @Published var postalCode: String = ""
+    @Published var city: String = ""
+    @Published var country: String = ""
+    @Published var weight: Double? = nil
+    @Published var birthDate: Date = Date()
+    @Published var profileImage: UIImage? = nil
+    @Published var errorMessage: String? = nil
+
+    private var firestoreManager = FirestoreManager()
+
+    func registerProfile() {
+        let profile = ProfileInformation(
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            postalCode: postalCode,
+            city: city,
+            country: country,
+            weight: weight,
+            birthDate: birthDate
+        )
+
+        if let image = profileImage {
+            uploadImage(image) { [weak self] url in
+                if let url = url {
+                    var updatedProfile = profile
+                    updatedProfile.profilePhotoURL = url.absoluteString
+                    self?.saveProfileToFirestore(updatedProfile)
+                } else {
+                    self?.errorMessage = "Error al subir la imagen."
+                }
+            }
+        } else {
+            saveProfileToFirestore(profile)
+        }
+    }
+
+    private func uploadImage(_ image: UIImage, completion: @escaping (URL?) -> Void) {
+        let storageRef = Storage.storage().reference().child("profileImages/\(UUID().uuidString).jpg")
+        if let imageData = image.jpegData(compressionQuality: 0.8) {
+            storageRef.putData(imageData, metadata: nil) { metadata, error in
+                if error != nil {
+                    completion(nil)
+                } else {
+                    storageRef.downloadURL { url, error in
+                        guard let downloadURL = url else {
+                            completion(nil)
+                            return
+                        }
+                        completion(downloadURL)
+                    }
+                }
+            }
+        } else {
+            completion(nil)
+        }
+    }
+
+    private func saveProfileToFirestore(_ profile: ProfileInformation) {
+        firestoreManager.createProfile(profile: profile) { [weak self] success, error in
+            if let error = error {
+                self?.errorMessage = error.localizedDescription
+            } else if success {
+                // Navegar a la siguiente vista o mostrar confirmación
+            }
+        }
+    }
+}
+
+/*
 import Foundation
 import SwiftUI
 import FirebaseStorage
@@ -113,3 +194,4 @@ class ProfileRegisterViewModel: ObservableObject {
     }
 
 }
+*/
